5ea4fd5660db5eb975826156ea1a1e5635e49012
Musa Naeem <musa.naeem@arbisoft.com>
2022-09-20 18:43:02 +0500

    removed extra code

--- BioForm/BioForm/settings.py
+++ BioForm/BioForm/settings.py
@@ -45,10 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'knox',
-    'api',
-    'corsheaders',
+    
     'profiles',
 ]
 
@@ -57,7 +54,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -148,6 +144,3 @@ LOGIN_REDIRECT_URL = '/profiles/index/'
 LOGIN_EXEMPT_URLS = (
     r'^profiles/register/$',
 )
-
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
