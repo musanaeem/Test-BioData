0302e50c80ca4ebaff9e388130c02da9458cea5e
Musa Naeem <musa.naeem@arbisoft.com>
2022-09-21 16:24:53 +0500

    Inactive users command turned to queryset

--- BioForm/profiles/management/commands/setuserinactive.py
+++ BioForm/profiles/management/commands/setuserinactive.py
@@ -7,16 +7,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         days_offline = 10
 
-        inactive_users = Account.objects.filter(last_login__lte=datetime.now()-timedelta(days=days_offline))
-
-        if inactive_users:
-            for user in inactive_users:
-                
-                count += 1
-                user.is_active = False
-
-                user.save()
+        inactive_users_queryset = Account.objects.filter(last_login__lte=datetime.now()-timedelta(days=days_offline))
+        inactive_users_count = inactive_users_queryset.count()
+        inactive_users_queryset.update(is_active=False)
         
-        print(f"{count} user(s) were set to inactive")
-
-
+        print(f"{inactive_users_count} user(s) were set to inactive")
